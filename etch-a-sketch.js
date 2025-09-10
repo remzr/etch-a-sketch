dc08365f2d156376e88207e3e424fc4c9b1d1a8a
remzr <110097561+remzr@users.noreply.github.com>
2025-09-10 21:21:42 +0200

    Create a canvas of 16x16 squares with flexbox, alway filling the screen

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,9 @@
-# etch-a-sketch.js
+function fillCanvas () {
+    for (let i = 0; i < 256; i++) {    
+        const canvasSquare = document.createElement("div");
+        canvasSquare.classList.add("canvasSquare");
+        canvasSection.appendChild(canvasSquare);
+    }    
+};
+
+fillCanvas();
