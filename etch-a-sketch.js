bcd0ab3d5eb932e5fd422ab3836ebee2eb71aee2
remzr <110097561+remzr@users.noreply.github.com>
2025-09-10 22:04:14 +0200

    Draw with hover effect

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -7,3 +7,18 @@ function fillCanvas () {
 };
 
 fillCanvas();
+function message () {
+    console.log("Hovered!");
+}
+
+//Called on Hover by event delegator (below)
+function changeHoverColor (eventTarget) {
+    eventTarget.classList.toggle("hovercolor");
+};
+
+//Global Eventlistener -> Event delegation
+document.body.addEventListener("mouseover", function(e) {
+  if (e.target.classList.contains("canvasSquare")) {
+    changeHoverColor(e.target);
+  }
+})
