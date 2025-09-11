107b32362316d29428de089243ce326b4e3cf637
remzr <110097561+remzr@users.noreply.github.com>
2025-09-11 21:22:33 +0200

    Create resize function with slider

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,29 @@
-function fillCanvas () {
-    for (let i = 0; i < 256; i++) {    
-        const canvasSquare = document.createElement("div");
-        canvasSquare.classList.add("canvasSquare");
-        canvasSection.appendChild(canvasSquare);
-    }    
+function resizeCanvas(canvasSize) {
+  //Rounding input to 4
+  while (canvasSize % 4 != 0) {
+    canvasSize++
+  }
+  fillCanvas(canvasSize);
+  //const button = document.getElementById(resizeButton)
 };
 
-fillCanvas();
-function message () {
-    console.log("Hovered!");
-}
+let canvasSize = 100;
+
+function fillCanvas (canvasSize) {
+  //Empty canvas
+  const element = document.getElementById("canvasSection");
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+  //Fill canvas
+  for (let i = 0; i < canvasSize; i++) {    
+      const canvasSquare = document.createElement("div");
+      canvasSquare.classList.add("canvasSquare");
+      canvasSection.appendChild(canvasSquare);
+  }    
+};
+
+fillCanvas(canvasSize);
 
 //Called on Hover by event delegator (below)
 function changeHoverColor (eventTarget) {
@@ -22,3 +36,11 @@ document.body.addEventListener("mouseover", function(e) {
     changeHoverColor(e.target);
   }
 })
+
+//Global Eventlistener -> Event delegation for slider
+document.body.addEventListener("mouseup", function(e) {
+  if (e.target.classList.contains("resizeSlider")) {
+    resizeCanvas(e.target.value);
+  }
+})
+
